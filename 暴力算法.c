de1fb2b150a889f48b8ca864c197e69b19ae8bf0
Thomson87 <77532643+Thomson87@users.noreply.github.com>
2021-01-25 19:12:34 +0800

    Add files via upload

---
+++ 暴力算法.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int main()
+//{
+//	int arr[] = { 1, 2, 3, 4, 5, 1, 2, 3, 4 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	for (int i = 0; i < sz; i++)
+//	{
+//		int count = 0;
+//		for (int j = 0; j < sz; j++)
+//		{
+//			if (arr[i] == arr[j])
+//			{
+//				count++;
+//			}
+//		}
+//		if (count == 1)
+//		{
+//			printf("%d\n", arr[i]);
+//			break;
+//		}
+//		return 0;
+//	}
+//	}
